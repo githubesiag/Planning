83d09b27202e2af00a784cc9246d9cd790eb7ae7
Fouad777 <Fouad777@PC>
2015-05-10 15:41:12 +0200

    ajout ! dans controlleur

--- Github_planning/src/controlleur/Controlleur.java
+++ Github_planning/src/controlleur/Controlleur.java
@@ -107,7 +107,7 @@ public class Controlleur extends javax.swing.JPanel {
     }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // Code a utiliser pour récuperer les infos des jtextfield
+        // Code a utiliser pour récuperer les infos des jtextfield !
     }//GEN-LAST:event_jButton1ActionPerformed
 
 
